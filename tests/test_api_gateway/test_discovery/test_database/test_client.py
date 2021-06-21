2d0390fc433417e522d7d58b436f524465f5752e
Alberto Amigo Alonso <albamigh@gmail.com>
2021-06-21 08:25:17 +0200

    issue #23
    - Extracted setUp in tests.

--- tests/test_api_gateway/test_discovery/test_database/test_client.py
+++ tests/test_api_gateway/test_discovery/test_database/test_client.py
@@ -16,49 +16,36 @@ from tests.utils import (
 
 
 class TestDiscoveryHandler(unittest.TestCase):
-    CONFIG_FILE_PATH = BASE_PATH / "config.yml"
+    def setUp(self) -> None:
+        config_file_path = BASE_PATH / "config.yml"
+        config = MinosConfig(config_file_path)
+        self.redis_client = MinosRedisClient(config=config)
 
     @mock.patch.dict(os.environ, {"DISCOVERY_SERVICE_DB_HOST": "redishost"})
     @mock.patch.dict(os.environ, {"DISCOVERY_SERVICE_DB_PORT": "8393"})
     @mock.patch.dict(os.environ, {"DISCOVERY_SERVICE_DB_PASSWORD": "SomePass"})
     def test_redis_client_connection(self):
-        config = MinosConfig(self.CONFIG_FILE_PATH)
         with self.assertRaises(Exception):
-            redis_cli = MinosRedisClient(config=config)
-            print(redis_cli.get_redis_connection())
+            print(self.redis_client.get_redis_connection())
 
     def test_redis_client_get_data(self):
-        config = MinosConfig(self.CONFIG_FILE_PATH)
-        redis_cli = MinosRedisClient(config=config)
-
-        data = redis_cli.get_data("a")
+        data = self.redis_client.get_data("a")
         self.assertEqual(data, {})
 
     def test_redis_client_set_data(self):
-        config = MinosConfig(self.CONFIG_FILE_PATH)
-        redis_cli = MinosRedisClient(config=config)
-
-        response = redis_cli.set_data("endpoint_name", {"test": "a"})
+        response = self.redis_client.set_data("endpoint_name", {"test": "a"})
         self.assertTrue(response)
 
     def test_redis_client_delete_unexisting_data(self):
-        config = MinosConfig(self.CONFIG_FILE_PATH)
-        redis_cli = MinosRedisClient(config=config)
-
-        response = redis_cli.delete_data("nokey")
+        response = self.redis_client.delete_data("nokey")
         self.assertFalse(response)
 
     def test_redis_client_delete_data(self):
-        config = MinosConfig(self.CONFIG_FILE_PATH)
-        redis_cli = MinosRedisClient(config=config)
-        response = redis_cli.set_data("endpoint_name", {"test": "a"})
+        response = self.redis_client.set_data("endpoint_name", {"test": "a"})
         self.assertTrue(response)
-        response = redis_cli.delete_data("endpoint_name")
+        response = self.redis_client.delete_data("endpoint_name")
         self.assertTrue(response)
 
     def test_redis_client_get_connection(self):
-        config = MinosConfig(self.CONFIG_FILE_PATH)
-        redis_cli = MinosRedisClient(config=config)
-
-        redis_conn = redis_cli.get_redis_connection()
+        redis_conn = self.redis_client.get_redis_connection()
         self.assertEqual(type(redis_conn).__name__, "Redis")
