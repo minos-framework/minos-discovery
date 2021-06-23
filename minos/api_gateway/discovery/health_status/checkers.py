1f9f78dabce75b027ff266f5eb34871206a8faa4
Sergio García Prado <sergio.garcia@clariteia.com>
2021-06-23 16:17:19 +0200

    ISSUE #32 * Improve the way to manage health status checking.

--- minos/api_gateway/discovery/health_status/checkers.py
+++ minos/api_gateway/discovery/health_status/checkers.py
@@ -5,9 +5,23 @@
 # Minos framework can not be copied and/or distributed without the express
 # permission of Clariteia SL.
 import logging
+from asyncio import (
+    gather,
+)
+from typing import (
+    Any,
+    NoReturn,
+)
+
+from aiohttp import (
+    ClientConnectorError,
+    ClientSession,
+)
+from yarl import (
+    URL,
+)
 
 from minos.api_gateway.common import (
-    ClientHttp,
     MinosConfig,
 )
 
@@ -30,57 +44,33 @@ class HealthStatusChecker:
 
         :return: TODO
         """
-        status_code = None
-        for key in self.redis_conn.scan_iter():
-            logger.info(f"Checking {key!r} health status...")
+        coroutines = (self._check_one(key) for key in self.redis_conn.scan_iter())
+        await gather(*coroutines)
 
-            try:
-                data = self.redis_cli.get_data(key)
-                try:
-                    status_code = await self._check_one(data)
-                finally:
-                    self._update_one(status_code, key, data)
-            finally:
-                pass
-
-        return True
-
-    # noinspection PyMethodMayBeStatic
-    async def _check_one(self, data):
-        status_code = None
-        if len(data) > 0 and "ip" in data and "name" in data:
-            url = "http://{0}/system/health".format(data["ip"])
-            if "port" in data:
-                if data["port"] is not None:
-                    url = "http://{0}:{1}/system/health".format(data["ip"], data["port"])
-
-            try:
-                async with ClientHttp() as client:
-                    response = await client.get(url=url)
-
-                status_code = response.status
-            except Exception as exc:
-                logger.warning(f"An exception was raised while checking {url!r} health status: {exc!r}")
-
-        return status_code
-
-    def _update_one(self, status_code, key, data):
+    async def _check_one(self, key: str):
+        logger.info(f"Checking {key!r} health status...")
         try:
-            # If response status code == 200
-            # set status to True else,
-            # set status to False
-            perform_update = False
-            if status_code == 200:
-                if data["status"] is not True:
-                    data["status"] = True
-                    perform_update = True
-            else:
-                if data["status"] is not False:
-                    data["status"] = False
-                    perform_update = True
+            # noinspection PyTypeChecker
+            data: dict[str, Any] = self.redis_cli.get_data(key)
+            alive = await self._query_health_status(**data)
+            self._update_one(alive, key, data)
+        except Exception as exc:
+            logger.warning(f"An exception was raised while checking {key!r}: {exc!r}")
 
-            if perform_update:
-                self.redis_cli.set_data(key, data)
+    # noinspection PyMethodMayBeStatic,PyUnusedLocal
+    async def _query_health_status(self, ip: str, port: int, **kwargs) -> bool:
+        url = URL.build(scheme="http", host=ip, port=port, path="/system/health")
 
-        finally:
-            pass
+        try:
+            async with ClientSession() as session:
+                async with session.get(url=url) as response:
+                    return response.ok
+        except ClientConnectorError:
+            return False
+
+    def _update_one(self, alive: bool, key: str, data: dict[str, Any]) -> NoReturn:
+        if alive == data["status"]:
+            return
+
+        data["status"] = alive
+        self.redis_cli.set_data(key, data)
